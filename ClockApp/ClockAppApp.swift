b79f95dcdc810dabad231b2dfe8c2fb7f53d5bbe
Ivan Trajanovski ruwido <ivan.trajanovski@ruwido.com>
2023-11-21 15:59:18 +0100

    Initial Commit

---
+++ ClockApp/ClockAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClockAppApp.swift
+//  ClockApp
+//
+//  Created by ivan  on 21.11.23.
+//
+
+import SwiftUI
+
+@main
+struct ClockAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ClockApp/ContentView.swift
@@ -0,0 +1,67 @@
+//
+//  ContentView.swift
+//  ClockApp
+//
+//  Created by ivan  on 21.11.23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var time = 0.0
+    @State private var isRunning = false
+    
+    let timer = Timer.publish(every: 0.1, on: .main, in: .common).autoconnect()
+    
+    var body: some View {
+        VStack {
+            Text("\(time, specifier: "%.1f")")
+                .foregroundStyle(.white)
+                .font(.system(size: 50))
+                .padding()
+            
+            HStack {
+                Button {
+                    time = 0.0
+                    isRunning = false
+                } label: {
+                    Circle()
+                        .frame(width: 90, height: 90)
+                        .foregroundStyle(.gray)
+                        .overlay(
+                            Text("Reset")
+                                .foregroundStyle(.white)
+                                .bold()
+                        )
+                }
+                .padding()
+                
+                Spacer()
+                
+                Button {
+                    isRunning.toggle()
+                } label: {
+                    Circle()
+                        .frame(width: 90, height: 90)
+                        .foregroundStyle(isRunning ? .red : .green)
+                        .overlay(
+                            Text(isRunning ? "Stop" : "Start")
+                                .foregroundStyle(.white)
+                                .bold()
+                        )
+                }
+                .padding()
+            }
+        }
+        .preferredColorScheme(.dark)
+        .onReceive(timer) { _ in
+            if isRunning == true {
+                time += 0.1
+            }
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
