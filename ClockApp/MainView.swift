cf978554a80cd4c64700d65b38f769cf1ca439a0
Ivan Trajanovski ruwido <ivan.trajanovski@ruwido.com>
2023-11-21 16:18:16 +0100

    create tab view and main view

--- ClockApp/MainView.swift
+++ ClockApp/MainView.swift
@@ -9,7 +9,13 @@ import SwiftUI
 
 struct MainView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        TabView {
+            StopwatchView()
+                .tabItem {
+                    Image(systemName: "stopwatch")
+                    Text("Stopwatch")
+                }
+        }
     }
 }
 
